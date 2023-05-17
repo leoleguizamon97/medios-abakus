057b87fc4ff4c475139e712e2915bfcf429a90c1
leoleguizamon <anlleguizamongu@unal.edu.co>
2023-05-17 10:31:49 -0500

    carga archivos balance y terceros dedicados

--- src/components/formulario-general.vue
+++ src/components/formulario-general.vue
@@ -15,10 +15,11 @@
 
 				<label for="terceros-path" class="form-label mb-2">Terceros:</label>
 				<input class="form-control" type="file" id="terceros-path" ref="terceros" placeholder="terceros.txt"
-					required @change="guardarArchivo">
+					required @change="guardarArchivoTerceros">
 
 				<label for="balance-path" class="form-label mb-2">Balance de prueba:</label>
-				<input class="form-control" type="file" id="balance-path" ref="balance" placeholder="balance.txt" required>
+				<input class="form-control" type="file" id="balance-path" ref="balance" placeholder="balance.txt" required
+					@change="guardarArchivoBalance">
 
 				<div class="d-flex my-2">
 					<button type="button" class="btn btn-primary w-50" title="Ejecutar" @click="recorrerArchivo">
@@ -28,11 +29,12 @@
 					<button type="button" class="btn btn-warning w-25 mx-2" title="descargar" @click="generarArchivo">
 						<i class="bi bi-download m-1"></i>
 					</button>
-					<button type="reset" class="btn btn-danger w-25" title="Limpiar" @click="this.error=false">
+					<button type="reset" class="btn btn-danger w-25" title="Limpiar" @click="this.error = false">
 						<i class="bi bi-stars m-1"></i>
 					</button>
 				</div>
-				<p class="text-danger-emphasis bg-danger-subtle border border-danger-subtle" v-show="error" style="text-align: center;">Hubo un error cargando el archivo. ¿Seguro que lo seleccionaste?</p>
+				<p class="text-danger-emphasis bg-danger-subtle border border-danger-subtle" v-show="error"
+					style="text-align: center;">Hubo un error cargando el archivo. ¿Seguro que lo seleccionaste?</p>
 
 			</div>
 		</div>
@@ -46,14 +48,18 @@ export default {
 		msg: String
 	},
 	methods: {
-		guardarArchivo(evento) {
+		guardarArchivoTerceros(evento) {
 			console.log(evento);
-			this.archivo = evento.target.files[0];
+			this.archivoTerceros = evento.target.files[0];
+		},
+		guardarArchivoBalance(evento) {
+			console.log(evento);
+			this.archivoBalance = evento.target.files[0];
 		},
 		recorrerArchivo() {
 			try {
 				const lector = new FileReader();
-				lector.readAsText(this.archivo);
+				lector.readAsText(this.archivoTerceros);
 				lector.onload = () => {
 					const contenido = lector.result;
 					const lineas = contenido.split('\n')
@@ -63,10 +69,28 @@ export default {
 						console.log(linea);
 					});
 				};
-				this.error=false
+				this.error = false
 				console.log('pasoooo');
 			} catch (error) {
-				this.error=true
+				this.error = true
+				console.error(error);
+			}
+			try {
+				const lector = new FileReader();
+				lector.readAsText(this.archivoBalance);
+				lector.onload = () => {
+					const contenido = lector.result;
+					const lineas = contenido.split('\n')
+					console.log(lineas.length);
+					lineas.forEach(linea => {
+						this.balance.push(linea)
+						console.log(linea);
+					});
+				};
+				this.error = false
+				console.log('pasoooo');
+			} catch (error) {
+				this.error = true
 				console.error(error);
 			}
 		},
@@ -77,7 +101,9 @@ export default {
 	data() {
 		return {
 			terceros: new Array(),
-			archivo: null,
+			balance: new Array(),
+			archivoTerceros: null,
+			archivoBalance: null,
 			error: false,
 		}
 	}
